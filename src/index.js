726d6b1e11cba47a2f0198e27a306a9e48c39f7a
Enivaldo Queiroz <enivaldo.jecq@gmail.com>
2022-12-04 12:08:57 -0300

    Componente com Propriedade

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import React from 'react'
 import './index.css'
 
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComponenteComParametro'
 
 ReactDOM.render(
-    <div>
+    <div id="app">
         <Primeiro></Primeiro>
+        <ComParametro 
+            titulo="Situação do Aluno"
+            subtitulo="Pedro" 
+            nota={9.3}/>
+        <ComParametro 
+            titulo="Situação do Aluno"
+            subtitulo="Maria" 
+            nota={6}/>
     </div>, 
     document.getElementById('root')
 )
