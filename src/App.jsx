1af47d8c0db4e010832151dc6e91159e23d13cda
Enivaldo Queiroz <enivaldo.jecq@gmail.com>
2022-12-05 21:19:40 -0300

    Desafio Número Aleatório - Resposta

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComponenteComParametro'
 import Fragmento from './components/basicos/Fragmento'
+import Aleatorio from "./components/basicos/Aleatorio";
 
 export default () => (
     <div id="app">
@@ -12,5 +13,6 @@ export default () => (
             subtitulo="Pedro"
             nota={9.3} />
         <Primeiro></Primeiro>
+        <Aleatorio min={1} max={60}/>
     </div>
 );
---
+++ src/components/basicos/Aleatorio.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default props =>{
+    const min = props.min
+    const max = props.max
+    const aleatorio = parseInt(Math.random() * (max - min) + max)
+    
+    return(
+        <div>
+            <h1>Valor Aleatório</h1>
+            <p><strong>Valor Minimo:</strong> { props.min } </p>
+            <p><strong>Valor Maximo:</strong> { props.max } </p>
+            <p><strong>Valor Escolhido:</strong> { aleatorio } </p>
+        </div>
+    )
+}
